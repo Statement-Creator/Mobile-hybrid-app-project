42bfaf4b39313aebc929d50134685b5e06879bed
Statement-Creator <39440738+Statement-Creator@users.noreply.github.com>
2018-06-27 22:02:59 -0400

    Update data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -14,5 +14,14 @@ export class DataService {
   getTopArtists(){
     return this.http.get('http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key={APIKEY}&limit=25&format=json').pipe(map(res=>res.json()));
   }
+  
+    getTopSongsOfArtist(artist){
+    return this.http.get('http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist='+artist+'&api_key={APIKEY}&format=json&limit=25').pipe(map(res=>res.json()));
+  }
+
+  getSongLyrics(song,artist){
+    return this.http.get('https://orion.apiseeds.com/api/music/lyric/'+artist+'/'+song+'?apikey={APIKEY}').pipe(map(res=>res.json()));
+  }
+
 
 }
